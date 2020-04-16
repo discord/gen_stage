85062ef0f655bbe2a12dd544e2c31d095c20bf26
James Mitchell <james.mitchell@discordapp.com>
2020-04-16 16:45:51 -0700

    be more elixiry

--- lib/gen_stage/metrics.ex
+++ lib/gen_stage/metrics.ex
@@ -7,7 +7,7 @@ defmodule GenStage.Metrics do
   Returns the registered name of the current process or nil.
   """
   def metrics_self_name() do
-    case :erlang.process_info(self(), :registered_name) do
+    case Process.info(self(), :registered_name) do
       {:registered_name, name} when is_atom(name) -> name
       _ -> nil
     end
